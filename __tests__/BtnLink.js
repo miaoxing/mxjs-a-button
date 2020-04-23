515b8f8c44daae4b1a428018d8597ac95eb4483e
twinh <twinhuang@qq.com>
2020-04-23 10:11:14 +0800

    test: 改用 enzyme 测试

--- __tests__/BtnLink.js
+++ __tests__/BtnLink.js
@@ -2,32 +2,16 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {mount} from 'enzyme';
 import {BtnLink} from '../';
-import {act} from 'react-dom/test-utils';
 import {MemoryRouter} from 'react-router';
 
-let container;
-
 describe('BtnLink', () => {
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
+  test('Text', () => {
+    const wrapper = mount(<MemoryRouter>
+      <BtnLink href="#">foo</BtnLink>
+    </MemoryRouter>);
 
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-  });
-
-  test('column', async () => {
-    await act(async () => {
-      ReactDOM.render(<MemoryRouter>
-        <BtnLink href="#">foo</BtnLink>
-      </MemoryRouter>, container);
-    });
-
-    const button = container.querySelector('a');
-    expect(button.textContent).toBe('foo');
+    expect(wrapper.find('a').text()).toBe('foo');
   });
 });
