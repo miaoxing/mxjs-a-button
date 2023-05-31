384981042227433a7219c6cbc2410ec3250ad238
twinh <twinhuang@qq.com>
2023-05-31 23:02:25 +0800

    test(antd5): 关闭 `hashed`

--- __tests__/BtnLink.js
+++ __tests__/BtnLink.js
@@ -1,20 +1,29 @@
 import {BtnLink} from '../';
 import {MemoryRouter} from 'react-router';
 import {render} from '@testing-library/react';
+import {ConfigProvider} from 'antd';
 
 describe('BtnLink', () => {
   test('renders correctly', () => {
-    const {container} = render(<MemoryRouter>
-      <BtnLink href="foo">bar</BtnLink>
-    </MemoryRouter>);
+    const {container} = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <BtnLink href="foo">bar</BtnLink>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
 
     expect(container).toMatchSnapshot();
   });
 
   test('text', () => {
-    const {getByTestId} = render(<MemoryRouter>
-      <BtnLink data-testid="foo" href="foo">bar</BtnLink>
-    </MemoryRouter>);
+    const {getByTestId} = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <BtnLink data-testid="foo" href="foo">bar</BtnLink>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
 
     expect(getByTestId('foo')).toMatchSnapshot();
   });
--- __tests__/NewBtn.js
+++ __tests__/NewBtn.js
@@ -1,20 +1,29 @@
 import {NewBtn} from '../';
 import {MemoryRouter} from 'react-router';
 import {render} from '@testing-library/react';
+import {ConfigProvider} from 'antd';
 
 describe('NewBtn', () => {
   test('default', () => {
-    const {container} = render(<MemoryRouter>
-      <NewBtn href="foo">bar</NewBtn>
-    </MemoryRouter>);
+    const {container} = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <NewBtn href="foo">bar</NewBtn>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
 
     expect(container).toMatchSnapshot();
   });
 
   test('text', () => {
-    const {container} = render(<MemoryRouter>
-      <NewBtn data-testid="foo" href="foo">bar</NewBtn>
-    </MemoryRouter>);
+    const {container} = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <MemoryRouter>
+          <NewBtn data-testid="foo" href="foo">bar</NewBtn>
+        </MemoryRouter>
+      </ConfigProvider>
+    );
 
     expect(container).toMatchSnapshot();
   });
